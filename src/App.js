97fd296d38f3cdca59b2937f119af184bc6950eb
leedbswo107 <ljyu1@naver.com>
2024-06-16 22:14:52 +0900

    feat: Header, Footer, Mypage Route set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import "./App.css";
+import './App.css';
 
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MyPage from './pages/MyPage';
 
 function App() {
   return (
     <div className="App">
-      <Routes>{/* <Route path="/" element={<Main />} /> */}</Routes>
+      <Header />
+      <Routes>
+        {/* <Route path="/" element={<Main />} /> */}
+        <Route path="/" element={<Main />} />
+        <Route path="/mypage/:nickName" element={<MyPage />} />
+        <Route path="*" element={<>잘못된 경로입니다</>} />
+      </Routes>
+
+      <Footer />
     </div>
   );
 }
