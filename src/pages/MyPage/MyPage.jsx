83d9e0fe578f18d4e7fa7cf1ea2d7d756b4802e6
leedbswo107 <ljyu1@naver.com>
2024-06-17 17:09:17 +0900

    feat: route changes & outlet added

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import style from '../../css/MyPage/MyPage.module.css';
 
 const MyPage = () => {
+  const [nickname, setNickname] = useState('홍길동');
+
   return (
-    <section>
+    <section className={style.MyPage}>
       <h2 hidden>MyPage</h2>
-      <div>
-        <p>안녕하세요, 000 님!</p>
+      <div className={style.Banner}>
+        <p>{`안녕하세요, ${nickname} 님!`}</p>
         <img src="/" alt="마이페이지 배너영역" />
       </div>
-      <div></div>
-      <aside>
-        <Link to="/my-page/bingo-coupon">빙고 / 쿠폰함</Link>
-        <Link to="/my-page/transaction-history">거래 내역</Link>
+      <aside className={style.SideBar}>
+        <Link to="/my-page/bingo-coupon/bingo">빙고 &#47; 쿠폰함</Link>
+        <Link to="/my-page/transaction-history/sale">거래 내역</Link>
         <Link to="/my-page/my-post">내가 쓴 게시글</Link>
         <Link to="/my-page/edit-info">회원정보 수정</Link>
         <Link to="/my-page/customer-service">고객 지원</Link>
       </aside>
+      <Outlet></Outlet>
     </section>
   );
 };
