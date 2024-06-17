960062f8e5719fcbfaa449c9750a5e482e0b7bb1
leedbswo107 <ljyu1@naver.com>
2024-06-17 17:08:19 +0900

    feat: bingoCoupon tab routing setting

--- src/pages/MyPage/BingoCoupon.jsx
+++ src/pages/MyPage/BingoCoupon.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 
 const BingoCoupon = () => {
   return (
     <section>
       <h3 hidden>BingoCoupon</h3>
-      빙고 쿠폰 영역
+      <nav>
+        <Link to={'bingo'}>빙고</Link>
+        <span>&#47;</span>
+        <Link to={'coupon'}>쿠폰함</Link>
+      </nav>
+      <Outlet></Outlet>
     </section>
   );
 };
