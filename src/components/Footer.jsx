98ea8fce814d32d959e4c4f2e621206c0ec616a1
mowha <ch0i_jiyoun@naver.com>
2024-06-18 11:27:27 +0900

    docs: footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 
+const styles = {
+  footer: {
+    width: "100%",
+    position: "absolute",
+    bottom: "0",
+  },
+  copyrightWrap: {
+    borderTop: "1px solid #B7B6B6",
+    textAlign: "center",
+    padding: "10px 0",
+    marginTop: "20px",
+  },
+  copyrightmemo: {
+    margin: "20px 0",
+    fontSize: " 12px",
+  },
+};
+
 const Footer = () => {
   return (
-    <footer>
-      <div className="copyrightWarp">
-        <p>
-          &copy; team_campick. All right reserved. The design refers to 'STAY
+    <footer style={styles.footer}>
+      <div className="copyrightWrap" style={styles.copyrightWrap}>
+        <p style={styles.copyrightmemo}>
+          &copy; team_campick. All rights reserved. The design refers to 'STAY
           FOLIO'.
         </p>
       </div>
