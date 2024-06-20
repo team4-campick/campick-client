b97360f9d0317ad6164025902dfc8c595531f6ae
leedbswo107 <ljyu1@naver.com>
2024-06-20 13:45:03 +0900

    feat: post 맵핑 기능 추가

--- src/pages/MyPage/MyPost.jsx
+++ src/pages/MyPage/MyPost.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-
+import PostCard from '../../components/MyPage/PostCard';
+import style from '../../css/MyPage/MyPost.module.css';
 const MyPost = () => {
   // const [postList, setPostList] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
   const postList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -14,11 +15,9 @@ const MyPost = () => {
     <section>
       <h3 hidden>MyPost</h3>
       <nav className="myPage_SubHeader">내가 쓴 게시글</nav>
-      <div className="postCon">
+      <div className={style.postCon}>
         {postList.map((post, i) => (
-          <article key={i}>
-            <h3>{post}</h3>
-          </article>
+          <PostCard key={i} post={post} />
         ))}
       </div>
     </section>
