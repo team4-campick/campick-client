0d0668796193cd53628df4d0e03743946c6efa8a
Michael <54614330+leedbswo107@users.noreply.github.com>
2024-06-20 19:20:08 +0900

    hotfix: MyPage -> Mypage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-import MainPage from "./pages/Mainpage";
+import Mainpage from "./pages/Mainpage";
 
 import MyPage from "./pages/MyPage/MyPage";
 import BingoCoupon from "./pages/MyPage/BingoCoupon";
