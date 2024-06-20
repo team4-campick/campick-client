79df105865424030df738706a533b08a0d10a424
leedbswo107 <ljyu1@naver.com>
2024-06-20 13:42:49 +0900

    feat: 쿠폰 구조 설정

--- src/components/MyPage/CouponCard.jsx
+++ src/components/MyPage/CouponCard.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import style from './CouponCard.module.css';
 
-const CouponCard = () => {
+const CouponCard = ({ coupon }) => {
   return (
-    <div>
-      <p>coupon type</p>
+    <div className={style.couponCard}>
+      <div className={style.statusCircle}>{coupon}</div>
+      <span className={style.issuanceCondition}>빙고 성공 할인권</span>
+      <p className={style.couponType}>10% 할인</p>
+      <span>상품 및 패키지 예약시 사용 가능한 할인 쿠폰</span>
+      <div className={style.couponInfo}>
+        <span>24.08.30 까지 사용가능</span>
+        <span className={style.caution}>
+          5만원 이상 구매 시 사용할 수 있습니다.
+        </span>
+      </div>
+      <u className={style.couponNum}>쿠폰 번호 확인</u>
     </div>
   );
 };
-
 export default CouponCard;
