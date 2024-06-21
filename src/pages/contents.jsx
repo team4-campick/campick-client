f9cf66e4fd4d3ea4cfc6088809a79d07a249d2fb
mowha <ch0i_jiyoun@naver.com>
2024-06-21 11:12:35 +0900

    add: contents jsx file

---
+++ src/pages/contents.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Styles from "../css/contents.module.css";
+
+const Contents = () => {
+  return (
+    <div className={Styles.container}>
+      <h1>Contents Component</h1>
+      {/* 추가적인 컴포넌트나 JSX 코드 작성 */}
+    </div>
+  );
+};
+
+export default Contents;
