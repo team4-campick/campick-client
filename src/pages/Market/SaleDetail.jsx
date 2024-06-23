ddee6345d8841edf56ac3997c4ed91319e0c1b66
newjinny <syj9437@gmail.com>
2024-06-23 21:30:32 +0900

    feat: 디테일페이지

--- src/pages/Market/SaleDetail.jsx
+++ src/pages/Market/SaleDetail.jsx
@@ -1,23 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "../../css/Market/SaleDetail.module.css";
+import { useParams } from "react-router-dom";
 
 const SaleDetail = () => {
+  const { id } = useParams();
+  const [salePostDetail, setSalePostDetail] = useState({});
+
+  const {
+    category,
+    productName,
+    region,
+    city,
+    condition,
+    price,
+    desc,
+    isNegotiable,
+  } = salePostDetail;
+
+  const API_BASE_URL = "http://localhost:8000/api";
+  const salePostsEndpoint = `${API_BASE_URL}/sale-posts/${id}`;
+
+  const fetchSalePostDetail = async () => {
+    try {
+      const response = await fetch(salePostsEndpoint, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      if (!data.result) {
+        return alert(data.message);
+      }
+      setSalePostDetail(data.salePost);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchSalePostDetail();
+  }, []);
+
   return (
     <section className={style.detailCon}>
       <h2 hidden>SaleDetail</h2>
-      <span>작성자</span>
-      <div>제품 종류</div>
-      <div>이미지 슬라이드 들어가는 곳</div>
-      <div>
-        <div>
-          <span>0000판매합니다.</span>
-          <span>서울</span>
-        </div>
-        <p>판매 희망가 : 5000원</p>
 
-        <div>상품 상태: </div>
-        <div>상품 설명</div>
+      <div className={style.productImg}>
+        <div className={style.productImgSlide}>이미지 슬라이드 들어가는 곳</div>
+        <span className={style.writer}>작성자</span>
       </div>
+      <div className={style.productInfo}>
+        <div>
+          <div className={style.productCategory}>{category}</div>
+          <p className={style.productName}>{productName}판매합니다.</p>
+          <span className={style.region}>
+            지역 : {region}/{city}
+          </span>
+        </div>
+        <div className={style.priceCon}>
+          <span>{isNegotiable ? "가격협의가능" : "가격협의불가"}</span>
+          <p className={style.productPrice}>판매 희망가 : {price}원</p>
+        </div>{" "}
+      </div>
+      <div>상품 상태 :{condition}</div>
+      <div>{desc}</div>
+
+      <button className={style.inquiryBtn}>문의하기</button>
     </section>
   );
 };
