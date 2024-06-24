f3bc8f65280054f9591e8239f026d9c386fade2e
leedbswo107 <ljyu1@naver.com>
2024-06-24 09:57:32 +0900

    feat: getMyPostList func

--- src/pages/MyPage/MyPost.jsx
+++ src/pages/MyPage/MyPost.jsx
@@ -2,21 +2,30 @@ import React, { useState, useEffect } from 'react';
 import PostCard from '../../components/MyPage/PostCard';
 import style from '../../css/MyPage/MyPost.module.css';
 const MyPost = () => {
-  // const [postList, setPostList] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-  const postList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const url = 'http://localhost:8000';
-  // useEffect(() => {
-  //   fetch(`${url}/post/:id`)
-  //     .then((res) => res.json)
-  //     .then((data) => setPostList(data));
-  // }, []);
-  console.log(postList);
+  const url = process.env.REACT_APP_SERVER_URL;
+  const [myPostList, setMyPostList] = useState([1, 2, 3, 4, 5, 6]);
+  const getMyPostList = async () => {
+    try {
+      const response = await fetch(`${url}/post/:id`);
+      const data = await response.json();
+      if (!data) {
+        alert('값이 없넹');
+      }
+      setMyPostList(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    getMyPostList();
+  }, []);
+  console.log(myPostList);
   return (
     <section>
       <h3 hidden>MyPost</h3>
       <nav className="myPage_SubHeader">내가 쓴 게시글</nav>
       <div className={style.postCon}>
-        {postList.map((post, i) => (
+        {myPostList.map((post, i) => (
           <PostCard key={i} post={post} />
         ))}
       </div>
