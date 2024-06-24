5e3298a481348907e779b51521babfb707ced726
leedbswo107 <ljyu1@naver.com>
2024-06-24 09:57:50 +0900

    feat: get product list

--- src/pages/MyPage/Sale.jsx
+++ src/pages/MyPage/Sale.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import style from '../../css/Market/Market.module.css';
+import SalePostCard from '../../components/Market/SalePostCard';
 const Sale = () => {
+  const url = process.env.REACT_APP_SERVER_URL;
+  const [salePosts, setSalePosts] = useState([]);
+
+  const getSalePostList = async () => {
+    try {
+      const response = await fetch(`${url}/api/sale-posts`);
+      const data = await response.json();
+      if (!data.result) {
+        return alert(data.message);
+      }
+
+      setSalePosts(data.salePosts);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getSalePostList();
+  }, []);
   return (
     <section>
       <h3 hidden>Sale</h3>
-      판매 영역
+      <div className={style.postCardList}>
+        {salePosts.map((post) => (
+          <SalePostCard key={post._id} post={post} />
+        ))}
+      </div>
     </section>
   );
 };
