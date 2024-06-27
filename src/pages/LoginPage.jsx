11c3795547aa56681d9ea39e5a162e0b57e2ca9c
kim soojin <verysoojini@gmail.com>
2024-06-27 17:47:57 +0900

    add : 로그아웃

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,37 +4,40 @@ import { Link, Navigate } from "react-router-dom";
 const url = process.env.REACT_APP_SERVER_URL;
 
 const LoginPage = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [message1, setMessage1] = useState('');
-  const [message2, setMessage2] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message1, setMessage1] = useState("");
+  const [message2, setMessage2] = useState("");
   const [redirect, setRedirect] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // isLoggedIn 상태 정의
 
   const login = async (e) => {
     e.preventDefault();
     console.log(username, password);
 
     if (!/^[a-zA-Z][a-zA-Z0-9]{3,}$/.test(username)) {
-      setMessage1('아이디는 4자 이상이어야 하며 영어로 시작해야 합니다.');
+      setMessage1("아이디는 4자 이상이어야 하며 영어로 시작해야 합니다.");
       return;
     } else {
-      setMessage1('');
+      setMessage1("");
     }
     if (password.length < 8) {
-      setMessage2('8자 이상이어야 합니다.');
+      setMessage2("8자 이상이어야 합니다.");
       return;
     } else {
-      setMessage2('');
+      setMessage2("");
     }
 
-    const loginDate = new Date().toISOString();
-    console.log(loginDate);
-    const response = await fetch(`${url}/login`, {
-      method: 'POST',
-      body: JSON.stringify({ username, password, loginDate }),
-      headers: { 'Content-Type': 'application/json' },
-      credentials: 'include',
-    });
+    try {
+      const loginDate = new Date().toISOString();
+      console.log(loginDate);
+
+      const response = await fetch(`${url}/login`, {
+        method: "POST",
+        body: JSON.stringify({ username, password, loginDate }),
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+      });
 
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
@@ -45,6 +48,7 @@ const LoginPage = () => {
 
       if (data.id) {
         setRedirect(true);
+        setIsLoggedIn(true);
       } else {
         if (data.message === "nouser") {
           setMessage1("사용자가 없습니다.");
@@ -59,6 +63,24 @@ const LoginPage = () => {
     }
   };
 
+  const logout = async () => {
+    try {
+      const response = await fetch(`${url}/logout`, {
+        method: "POST",
+        credentials: "include",
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      setIsLoggedIn(false);
+      setRedirect(false);
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
+
   if (redirect) {
     return <Navigate to="/" />;
   }
@@ -67,27 +89,31 @@ const LoginPage = () => {
     <main className={`mw ${style.register}`}>
       <h2>로그인</h2>
 
-      <form onSubmit={login}>
-        <input
-          type="text"
-          placeholder="사용자이름"
-          value={username}
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-        <span>{message1}</span>
-        <input
-          type="password"
-          placeholder="패스워드"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <span>{message2}</span>
-        <button type="submit">로그인</button>
-      </form>
+      {isLoggedIn ? (
+        <button onClick={logout}>로그아웃</button>
+      ) : (
+        <form onSubmit={login}>
+          <input
+            type="text"
+            placeholder="사용자이름(ID)"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <span>{message1}</span>
+          <input
+            type="password"
+            placeholder="패스워드"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <span>{message2}</span>
+          <button type="submit">로그인</button>
+        </form>
+      )}
       <p>
         계정이 없으신가요? <Link to="/register">회원가입</Link>
       </p>
