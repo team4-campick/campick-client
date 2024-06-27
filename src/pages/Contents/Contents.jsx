d5969491c15eac9790153cf13bee089f3c54c00c
mowha <ch0i_jiyoun@naver.com>
2024-06-27 14:10:42 +0900

    add: file contents

--- src/pages/Contents/Contents.jsx
+++ src/pages/Contents/Contents.jsx
@@ -4,17 +4,15 @@ import style from "../../css/Contents/contents.module.css";
 
 const Contents = () => {
   return (
-    <>
-      <section className={style.container}>
-        <h3 hidden>contents</h3>;
-        <nav className={style.contentSubHeader}>
-          <Link to={"contentsBlog"}>BLOG</Link>
-          <span>&nbsp;&#47;&nbsp;</span>
-          <Link to={"contentsVideo"}>VIDEO</Link>
-        </nav>
-        <Outlet></Outlet>
-      </section>
-    </>
+    <section className={style.container}>
+      <h3 hidden>contents</h3>
+      <nav className={style.contentSubHeader}>
+        <Link to={"contentsBlog"}>BLOG</Link>
+        <span>&nbsp;&#47;&nbsp;</span>
+        <Link to={"contentsVideo"}>VIDEO</Link>
+      </nav>
+      <Outlet></Outlet>
+    </section>
   );
 };
 
--- src/pages/Contents/ContentsVideo.jsx
+++ src/pages/Contents/ContentsVideo.jsx
@@ -1,20 +1,12 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
 import style from "../../css/Contents/contentsBlog.module.css";
 
 const ContentsVideo = () => {
   return (
-    <>
-      <section className={style.container}>
-        <h3 hidden>contents</h3>;
-        <nav className={style.contentSubHeader}>
-          <Link to={"contentsBlog"}>BLOG</Link>
-          <span>&nbsp;&#47;&nbsp;</span>
-          <Link to={"contentsVideo"}>VIDEO</Link>
-        </nav>
-        <Outlet></Outlet>
-      </section>
-    </>
+    <section>
+      <h3 hidden>contents Video</h3>
+      <div className={style.videoWarp}>a</div>
+    </section>
   );
 };
 
