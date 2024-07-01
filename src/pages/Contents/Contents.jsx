8100e7c5a8496cac7335d7c48b73f31f9e454ddb
mowha <ch0i_jiyoun@naver.com>
2024-07-01 14:35:22 +0900

    feat: style에 들어갈 파일명 변경으로 인한 변경

--- src/pages/Contents/Contents.jsx
+++ src/pages/Contents/Contents.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, Outlet, useLocation } from "react-router-dom";
-import cateogryStyle from "../../css/Cateogry.module.css";
+import cateogryStyle from "../../css/Category.module.css";
 
 const Contents = () => {
   const location = useLocation();
