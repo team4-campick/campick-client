6949826ce29e74eb81b00c1faefa1393106447d3
mowha <ch0i_jiyoun@naver.com>
2024-07-01 11:46:22 +0900

    feat: 파일 분할로 인한 변경점

---
+++ src/pages/Event/Event.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link, Outlet, useLocation } from "react-router-dom";
+import cateogryStyle from "../../css/Cateogry.module.css";
+import style from "../../css/Event/event.module.css";
+
+const Event = () => {
+  const location = useLocation();
+
+  return (
+    <section className={cateogryStyle.pageWarp}>
+      <h3 hidden>Event</h3>
+      <nav className={cateogryStyle.pageSubHeader}>
+        <Link
+          to="eventProceeding"
+          className={
+            location.pathname === "/event/eventProceeding" ? style.active : ""
+          }
+        >
+          PROCEEDING
+        </Link>
+        <Link
+          to="eventFinished"
+          className={
+            location.pathname === "/event/eventFinished" ? style.active : ""
+          }
+        >
+          FINISHED
+        </Link>
+      </nav>
+      <Outlet />
+    </section>
+  );
+};
+
+export default Event;
