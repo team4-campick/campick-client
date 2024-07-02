5363816f4ea186776ba98379ba473cc68cd5459e
mowha <ch0i_jiyoun@naver.com>
2024-07-02 11:12:38 +0900

    feat: h3 to h2 , commnet style tag

--- src/pages/Event/Event.jsx
+++ src/pages/Event/Event.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link, Outlet, useLocation } from "react-router-dom";
 import categoryStyle from "../../css/Category.module.css";
-import style from "../../css/Event/event.module.css";
+// import style from "../../css/Event/event.module.css";
 
 const Event = () => {
   const location = useLocation();
 
   return (
     <section className={categoryStyle.pageWarp}>
-      <h3 hidden>Event</h3>
+      <h2 hidden>Event</h2>
       <nav className={categoryStyle.pageSubHeader}>
         <Link
           to="eventProceeding"
