d9078e0e66056bb2d294c802a996d5f531c564c6
mowha <ch0i_jiyoun@naver.com>
2024-07-02 20:16:46 +0900

    feat: add active, navlink for style

--- src/pages/Event/Event.jsx
+++ src/pages/Event/Event.jsx
@@ -1,35 +1,32 @@
 import React from "react";
-import { Link, Outlet, useLocation } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import categoryStyle from "../../css/Category.module.css";
-// import style from "../../css/Event/event.module.css";
 
 const Event = () => {
-  const location = useLocation();
-
   return (
     <section className={categoryStyle.pageWarp}>
       <h2 hidden>Event</h2>
       <nav className={categoryStyle.pageSubHeader}>
-        <Link
+        <NavLink
           to="eventProceeding"
-          className={
-            location.pathname === "/event/eventProceeding"
-              ? categoryStyle.active
-              : ""
+          className={({ isActive }) =>
+            isActive
+              ? `${categoryStyle.active} ${categoryStyle.pageSubHeaderLink}`
+              : categoryStyle.pageSubHeaderLink
           }
         >
           PROCEEDING
-        </Link>
-        <Link
+        </NavLink>
+        <NavLink
           to="eventFinished"
-          className={
-            location.pathname === "/event/eventFinished"
-              ? categoryStyle.active
-              : ""
+          className={({ isActive }) =>
+            isActive
+              ? `${categoryStyle.active} ${categoryStyle.pageSubHeaderLink}`
+              : categoryStyle.pageSubHeaderLink
           }
         >
           FINISHED
-        </Link>
+        </NavLink>
       </nav>
       <Outlet />
     </section>
