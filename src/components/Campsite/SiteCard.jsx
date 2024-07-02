ae33665b18fb9e2457305a5d7610175a8d397693
leedbswo107 <ljyu1@naver.com>
2024-07-03 03:49:31 +0900

    feat: structure change & data null check

--- src/components/Campsite/SiteCard.jsx
+++ src/components/Campsite/SiteCard.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 const SiteCard = ({ site }) => {
   const navigate = useNavigate();
+  console.log("site", site);
   const { doNm, sigunguNm, firstImageUrl, facltNm, contentId } = site;
   return (
     <div
@@ -14,14 +15,23 @@ const SiteCard = ({ site }) => {
     >
       <figure>
         <figcaption>
-          <p className={style.info}>
-            <span>{`${doNm} ${sigunguNm}`}</span>
-            <span>{facltNm}</span>
-          </p>
-          <span className={style.score}>점수</span>
+          {/* <p className={style.info}> */}
+          <span>{facltNm}</span>
+          <span>{`${doNm} ${sigunguNm}`}</span>
+          {/* </p> */}
+          {/* <span className={style.score}>점수</span> */}
         </figcaption>
         <div className={style.imgCon}>
-          <img src={firstImageUrl} alt={facltNm} />
+          {firstImageUrl ? (
+            <img src={firstImageUrl} alt={facltNm} />
+          ) : (
+            <img
+              src={
+                "https://img.freepik.com/free-vector/camping-with-caravan-illustration_23-2148671151.jpg?t=st=1719940461~exp=1719944061~hmac=cd44c5d77dbbd15763529c53c68fe2ac04a2b679627fa15f3602451ff14e0ea7&w=1380"
+              }
+              alt={"no-img"}
+            />
+          )}
         </div>
       </figure>
     </div>
