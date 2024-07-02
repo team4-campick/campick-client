69cb55709c1ef9db7593c7381b63a7e2bfae1bfd
leedbswo107 <ljyu1@naver.com>
2024-07-02 12:03:18 +0900

    add: constant scoreMsg

---
+++ src/constants/scoreMsg.js
@@ -0,0 +1,9 @@
+const SCORE_MSG = Object.freeze({
+  DEFAULT: "캠핑장은 어떠셨나요?",
+  ONE: "별로였어요.",
+  TWO: "아쉬웠어요.",
+  THREE: "그저 그랬어요.",
+  FOUR: "좋았어요.",
+  FIVE: "대박 완전 좋았어요.",
+});
+export { SCORE_MSG };
