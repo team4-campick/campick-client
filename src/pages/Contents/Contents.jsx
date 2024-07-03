d2b87ec24f1ea6fa1ce7abb09cff253066ac04d8
mowha <ch0i_jiyoun@naver.com>
2024-07-03 14:31:44 +0900

    feat: category underline bar handle hover function

--- src/pages/Contents/Contents.jsx
+++ src/pages/Contents/Contents.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import categoryStyle from "../../css/Category.module.css";
 
 const Contents = () => {
+  const [hoverUnderlineStyle, setHoverUnderlineStyle] = useState({});
+  const [activeUnderlineStyle, setActiveUnderlineStyle] = useState({});
+  const navRef = useRef(null);
+
+  const handleHover = (e) => {
+    const link = e.target;
+    const { offsetLeft, offsetWidth } = link;
+    setHoverUnderlineStyle({
+      left: offsetLeft,
+      width: offsetWidth,
+    });
+  };
+
+  const handleMouseLeave = () => {
+    setHoverUnderlineStyle(activeUnderlineStyle);
+  };
+
+  useEffect(() => {
+    if (navRef.current) {
+      const activeLink = navRef.current.querySelector(
+        `.${categoryStyle.active}`
+      );
+      if (activeLink) {
+        const { offsetLeft, offsetWidth } = activeLink;
+        const style = {
+          left: offsetLeft,
+          width: offsetWidth,
+        };
+        setHoverUnderlineStyle(style);
+        setActiveUnderlineStyle(style);
+      }
+    }
+  }, []);
+
   return (
     <section className={categoryStyle.pageWarp}>
       <h3 hidden>contents</h3>
-      <nav className={categoryStyle.pageSubHeader}>
+      <nav className={categoryStyle.pageSubHeader} ref={navRef}>
         <NavLink
           to="contentsBlog"
           className={({ isActive }) =>
@@ -14,6 +48,8 @@ const Contents = () => {
               ? `${categoryStyle.active} ${categoryStyle.pageSubHeaderLink}`
               : categoryStyle.pageSubHeaderLink
           }
+          onMouseEnter={handleHover}
+          onMouseLeave={handleMouseLeave}
         >
           BLOG
         </NavLink>
@@ -24,9 +60,13 @@ const Contents = () => {
               ? `${categoryStyle.active} ${categoryStyle.pageSubHeaderLink}`
               : categoryStyle.pageSubHeaderLink
           }
+          onMouseEnter={handleHover}
+          onMouseLeave={handleMouseLeave}
         >
           VIDEO
         </NavLink>
+
+        <div className={categoryStyle.underline} style={hoverUnderlineStyle} />
       </nav>
       <Outlet />
     </section>
