2c00740c569ad94456cb99d18a5d62e5738db49e
leedbswo107 <ljyu1@naver.com>
2024-07-04 02:51:20 +0900

    remove: unnecessary store

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-
-import inquiry from "./inquiryStore";
 import user from "./userStore";
 
 export const store = configureStore({
   reducer: {
-    inquiry: inquiry.reducer,
     user: user.reducer,
   },
 });
