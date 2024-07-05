097f787ad9a9b32cf7589acadd3b22817f8dfb78
mowha <ch0i_jiyoun@naver.com>
2024-07-05 09:28:21 +0900

    feat: category css를 위한 useEffect location 의존성처리

--- src/pages/Contents/Contents.jsx
+++ src/pages/Contents/Contents.jsx
@@ -1,41 +1,39 @@
 import React, { useRef, useState, useEffect } from "react";
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useLocation } from "react-router-dom";
 import categoryStyle from "../../css/Category.module.css";
 
 const Contents = () => {
-  const [hoverUnderlineStyle, setHoverUnderlineStyle] = useState({});
-  const [activeUnderlineStyle, setActiveUnderlineStyle] = useState({});
+  const [underlineStyle, setUnderlineStyle] = useState({});
   const navRef = useRef(null);
+  const location = useLocation();
 
-  const handleHover = (e) => {
-    const link = e.target;
-    const { offsetLeft, offsetWidth } = link;
-    setHoverUnderlineStyle({
-      left: offsetLeft,
-      width: offsetWidth,
-    });
-  };
-
-  const handleMouseLeave = () => {
-    setHoverUnderlineStyle(activeUnderlineStyle);
-  };
-
-  useEffect(() => {
+  const updateUnderlineStyle = () => {
     if (navRef.current) {
       const activeLink = navRef.current.querySelector(
         `.${categoryStyle.active}`
       );
       if (activeLink) {
         const { offsetLeft, offsetWidth } = activeLink;
-        const style = {
+        setUnderlineStyle({
           left: offsetLeft,
           width: offsetWidth,
-        };
-        setHoverUnderlineStyle(style);
-        setActiveUnderlineStyle(style);
+        });
       }
     }
-  }, []);
+  };
+
+  const handleHover = (e) => {
+    const link = e.target;
+    const { offsetLeft, offsetWidth } = link;
+    setUnderlineStyle({
+      left: offsetLeft,
+      width: offsetWidth,
+    });
+  };
+
+  useEffect(() => {
+    updateUnderlineStyle();
+  }, [location]);
 
   return (
     <section className={categoryStyle.pageWarp}>
@@ -49,7 +47,6 @@ const Contents = () => {
               : categoryStyle.pageSubHeaderLink
           }
           onMouseEnter={handleHover}
-          onMouseLeave={handleMouseLeave}
         >
           BLOG
         </NavLink>
@@ -61,12 +58,11 @@ const Contents = () => {
               : categoryStyle.pageSubHeaderLink
           }
           onMouseEnter={handleHover}
-          onMouseLeave={handleMouseLeave}
         >
           VIDEO
         </NavLink>
 
-        <div className={categoryStyle.underline} style={hoverUnderlineStyle} />
+        <div className={categoryStyle.underline} style={underlineStyle} />
       </nav>
       <Outlet />
     </section>
