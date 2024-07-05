2a2fbf558349d98321635a877d4656c4e7954e01
mowha <ch0i_jiyoun@naver.com>
2024-07-05 11:47:57 +0900

    feat: video background play

--- src/pages/Contents/ContentsVideo.jsx
+++ src/pages/Contents/ContentsVideo.jsx
@@ -1,88 +1,36 @@
 import React from "react";
 import style from "../../css/Contents/contentsVideo.module.css";
 
-// 서버에 넣어야할 듯?
-const videos = [
-  {
-    id: 1,
-    title: "제주시 호텔",
-    description:
-      "As opposed to using 'Content here, content here', making it look like...",
-    user: "Campick",
-    userImage: "path_to_user_image",
-    videoSrc: "path_to_video",
-  },
-  {
-    id: 2,
-    title: "제주시 호텔",
-    description:
-      "As opposed to using 'Content here, content here', making it look like...",
-    user: "Campick",
-    userImage: "path_to_user_image",
-    videoSrc: "path_to_video",
-  },
-  {
-    id: 3,
-    title: "제주시 호텔",
-    description:
-      "As opposed to using 'Content here, content here', making it look like...",
-    user: "Campick",
-    userImage: "path_to_user_image",
-    videoSrc: "path_to_video",
-  },
-  {
-    id: 4,
-    title: "제주시 호텔",
-    description:
-      "As opposed to using 'Content here, content here', making it look like...",
-    user: "Campick",
-    userImage: "path_to_user_image",
-    videoSrc: "path_to_video",
-  },
-  {
-    id: 5,
-    title: "제주시 호텔",
-    description:
-      "As opposed to using 'Content here, content here', making it look like...",
-    user: "Campick",
-    userImage: "path_to_user_image",
-    videoSrc: "path_to_video",
-  },
-  {
-    id: 6,
-    title: "제주시 호텔",
-    description:
-      "As opposed to using 'Content here, content here', making it look like...",
-    user: "Campick",
-    userImage: "path_to_user_image",
-    videoSrc: "path_to_video",
-  },
-
-  // 다른 영상 데이터들 추가
-];
+const importAll = (r) => r.keys().map(r);
+const videos = importAll(
+  require.context(
+    "../../../public/images/ContentsVideo",
+    false,
+    /\.(mp4|webm)$/
+  )
+);
 
 const ContentsVideo = () => {
   return (
     <div className={style.ContentsVideo}>
-      {videos.map((video) => (
-        <div key={video.id} className={style.videoBox}>
-          <video
-            className={style.video}
-            src={video.videoSrc}
-            muted
-            autoPlay
-            loop
-          />
+      {videos.map((videoSrc, index) => (
+        <div key={index} className={style.videoBox}>
+          <video className={style.video} src={videoSrc} muted autoPlay loop />
           <div className={style.infoBox}>
-            <strong className={style.videoTitle}>{video.title}</strong>
-            <p className={style.videoDescription}>{video.description}</p>
+            {/* <strong className={style.videoTitle}>
+              제주시 호텔 {index + 1}
+            </strong> */}
+            <p className={style.videoDescription}>
+              As opposed to using 'Content here, content here', making it look
+              like...
+            </p>
             <div className={style.userBox}>
               <img
-                src={video.userImage}
-                alt={video.user}
+                src="/path_to_user_image"
+                alt="Campick"
                 className={style.userImage}
               />
-              <span className={style.videoUser}>{video.user}</span>
+              <span className={style.videoUser}>Campick</span>
             </div>
           </div>
         </div>
